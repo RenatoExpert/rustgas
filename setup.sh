cba593c39ee4befbf8ddcb5ce71fbbf254fa9113
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2023-10-15 00:54:30 -0300

    install aga tables on /var/rustgas

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo Installing...
+
+echo Making directory /var/rustgas 
+sudo mkdir -pv /var/rustgas
+sudo chmod 777 /var/rustgas
+echo Ok!
+
+echo AGA Tables Reference
+echo Downloading...
+git submodule update
+cp -Rv aga_tables /var/rustgas
+echo Ok!
+
+echo Finished
