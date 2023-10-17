57e622d6dc94b477f0ae0f17d55cebe530c8a0db
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2023-10-16 23:48:21 -0300

    creating types unary, binary and enum for these two types of parameter set

--- src/aga8/blockdata.rs
+++ src/aga8/blockdata.rs
@@ -5,6 +5,14 @@ use std::string::String;
 use std::io::Read;
 use std::collections::HashMap;
 
+type unary = HashMap<u8, f64>;
+type binary = HashMap<(u8, u8), f64>;
+#[derive(Debug)]
+enum Parameter {
+	Unary(unary),
+	Binary(binary)
+}
+
 fn read_table(table_num: u8) -> Value {
 	let path: String = format!("/var/rustgas/aga_tables/AGA8/table{}.json", table_num);
 	let mut file = File::open(path).unwrap();
@@ -14,7 +22,7 @@ fn read_table(table_num: u8) -> Value {
 	return table;
 }
 
-fn get_table4() -> (HashMap<u8, f64>) {
+fn get_table4() -> (unary) {
 	let table_4 = read_table(4);
 	let mut A: HashMap<u8, f64> = HashMap::new();
 	for cid in 1..=58 {
@@ -24,16 +32,16 @@ fn get_table4() -> (HashMap<u8, f64>) {
 	return (A);
 }
 
-fn get_table5() -> (HashMap<u8, f64>, HashMap<u8, f64>, HashMap<u8, f64>, HashMap<u8, f64>, HashMap<u8, f64>, HashMap<u8, f64>, HashMap<u8, f64>) {
+fn get_table5() -> (unary, unary, unary, unary, unary, unary, unary) {
 	let table_5 = read_table(5);
 	let default: f64 = *&table_5["default_value"].clone().as_f64().unwrap();
-	let mut QIB: HashMap<u8, f64> = HashMap::new();
-	let mut HIB: HashMap<u8, f64> = HashMap::new();
-	let mut RKIB: HashMap<u8, f64> = HashMap::new();
-	let mut EIB: HashMap<u8, f64> = HashMap::new();
-	let mut WIB: HashMap<u8, f64> = HashMap::new();
-	let mut CMWB: HashMap<u8, f64> = HashMap::new();
-	let mut MIB: HashMap<u8, f64> = HashMap::new();
+	let mut QIB: unary = HashMap::new();
+	let mut HIB: unary = HashMap::new();
+	let mut RKIB: unary = HashMap::new();
+	let mut EIB: unary = HashMap::new();
+	let mut WIB: unary = HashMap::new();
+	let mut CMWB: unary = HashMap::new();
+	let mut MIB: unary = HashMap::new();
 	let fetch = | cid: u8, parameter: &str | -> f64 {
 		let index: String = cid.to_string();
 		let value: f64 = *&table_5["data"][index][parameter].clone().as_f64().unwrap_or(default);
@@ -51,13 +59,13 @@ fn get_table5() -> (HashMap<u8, f64>, HashMap<u8, f64>, HashMap<u8, f64>, HashMa
 	return (QIB, HIB, RKIB, EIB, WIB, CMWB, MIB);
 }
 
-fn get_table6() -> (HashMap<(u8, u8), f64>, HashMap<(u8, u8), f64>, HashMap<(u8, u8), f64>, HashMap<(u8, u8), f64>) {
+fn get_table6() -> (binary, binary, binary, binary) {
 	let table_6 = read_table(6);
 	let default: f64 = *&table_6["default_value"].clone().as_f64().unwrap();
-	let mut BUIJB: HashMap<(u8, u8), f64> = HashMap::new();
-	let mut BKIJB: HashMap<(u8, u8), f64> = HashMap::new();
-	let mut BEIJB: HashMap<(u8, u8), f64> = HashMap::new();
-	let mut BWIJB: HashMap<(u8, u8), f64> = HashMap::new();
+	let mut BUIJB: binary = HashMap::new();
+	let mut BKIJB: binary = HashMap::new();
+	let mut BEIJB: binary = HashMap::new();
+	let mut BWIJB: binary = HashMap::new();
 	let fetch = | i_num: u8, j_num: u8, parameter: &str | -> f64 {
 		let i: String = i_num.to_string();
 		let j: String = j_num.to_string();
