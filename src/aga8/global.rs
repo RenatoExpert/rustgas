4520e2ddce4f9123c034faf744d8cb751affd6e3
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2023-10-17 00:44:38 -0300

    implementing Clone derive for Parameter

--- src/aga8/global.rs
+++ src/aga8/global.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 pub type unary = HashMap<u8, f64>;
 pub type binary = HashMap<(u8, u8), f64>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Parameter {
 	Unary(unary),
 	Binary(binary)
