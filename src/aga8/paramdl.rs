c3b60dd06dbd0c9ae14a6f42e20427fd53897845
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2023-10-17 00:33:17 -0300

    declaring some variables to params

--- src/aga8/paramdl.rs
+++ src/aga8/paramdl.rs
@@ -16,6 +16,13 @@ fn get_ncc(cid: HashMap<u8, f64>) -> u8 {
 
 pub fn paramdl(cid: HashMap<u8, f64>, block_data: HashMap<&str, global::Parameter>) {
 	let ncc: u8 = get_ncc(cid);
-	dbg!(ncc);
+	let told: f64 = 0.0;
+	let rgas = 8.31451e-3;
+	let tlow: f64 = 0.0;
+	let thigh: f64 = 10000.0;
+	let plow: f64 = 0.5e-9;
+	let phigh: f64 = 275.0;
+	let dhigh: f64 = 12.0;
+	//	Constants from block data
 }
 
