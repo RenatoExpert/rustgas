c5d88dd99ea05eed0f4d69cb0bc4777a3d72522d
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2023-10-17 10:11:21 -0300

    new subfunction to get constants from block_data

--- src/aga8/paramdl.rs
+++ src/aga8/paramdl.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use crate::aga8::global;
-use global::unary;
+use global::{Parameter, unary};
 
 fn get_ncc(cid: HashMap<u8, f64>) -> u8 {
 	let mut ncc: u8 = 0;
@@ -15,6 +15,21 @@ fn get_ncc(cid: HashMap<u8, f64>) -> u8 {
 	return ncc;
 }
 
+fn get_constants(block_data: HashMap<&str, global::Parameter>, ncc: u8) -> (unary, unary) {
+	let cmw: unary = HashMap::new();
+	let rki: unary = HashMap::new();
+	let extract = |param, j| -> f64 {
+		return block_data.get(param).unwrap().capture_unary(j);
+	};
+	for j in 1..=ncc {
+		let res = extract("CMWB", j);
+		dbg!(res);
+		//cmw.insert(j, Parameter::Unary(block_data["CMWB"]).get(j));
+	}
+	dbg!(cmw.clone());
+	return (cmw, rki);
+}
+
 pub fn paramdl(cid: HashMap<u8, f64>, block_data: HashMap<&str, global::Parameter>) {
 	let ncc: u8 = get_ncc(cid);
 	let told: f64 = 0.0;
@@ -26,5 +41,6 @@ pub fn paramdl(cid: HashMap<u8, f64>, block_data: HashMap<&str, global::Paramete
 	let dhigh: f64 = 12.0;
 	//	Constants from block data
 	let mwx: f64 = 0.0;
+	let (cmw, rki) = get_constants(block_data, ncc);
 }
 
