e461980efc9836cbcb7cfabde73570623edaa9b4
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2023-10-17 20:34:07 -0300

    calc mixturesize

--- src/aga8/chardl.rs
+++ src/aga8/chardl.rs
@@ -24,12 +24,36 @@ fn calc_molarmass(xi: Unary, params: ParameterSet, ncc: u8) -> f64 {
 	return mwx;
 }
 
+fn calc_mixturesize(xi: Unary, params: ParameterSet, ncc: u8) -> f64 {
+	let k: f64;
+	let mut sum_a: f64 = 0.0;
+	for i in 1..=ncc {
+		let Ki = params["RKI"].capture_unary(i);
+		sum_a += xi[&i] + Ki.powf(5./2.);
+	}
+	let part_a = sum_a.powf(2.);
+	let mut sum_b = 0.0;
+	for i in 1..=ncc-1 {
+		for j in i+1..=ncc {
+			let Ki = params["RKI"].capture_unary(i);
+			let Kj = params["RKI"].capture_unary(j);
+			let Kij = params["BKIJ"].capture_binary(i, j);
+			sum_b += xi[&i] * xi[&j] * (Kij.powf(5.) - 1.) * (Ki * Kj).powf(5./2.);
+		}
+	}
+	let part_b = 2.0 * sum_b;
+	let kp5: f64 = part_a + part_b;
+	k = kp5.powf(1./5.);
+	return k;
+}
+
 //	Returns compressibility and density for base state (zb, db)
 pub fn chardl(cid: Unary, params: ParameterSet) -> (f64, f64) {
 	let ncc: u8 = params["NCC"].unwrap_counter();
 	let xi: Unary = get_mole_fractions(ncc, cid);
 	let mwx: f64 = calc_molarmass(xi.clone(), params.clone(), ncc);
-	dbg!(mwx);
+	let k: f64 = calc_mixturesize(xi.clone(), params.clone(), ncc); 
+	dbg!(k);
 	return (0.0, 0.0);
 }
 
