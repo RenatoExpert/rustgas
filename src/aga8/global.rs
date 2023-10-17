296af3ee8277b0e195cbab015084f30f243ec377
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2023-10-17 10:08:32 -0300

    making impl functions for Parameter enum

--- src/aga8/global.rs
+++ src/aga8/global.rs
@@ -8,3 +8,19 @@ pub enum Parameter {
 	Unary(unary),
 	Binary(binary)
 }
+
+impl Parameter {
+	pub fn capture_unary(&self, i: u8) -> f64 {
+		match self {
+			Parameter::Unary(params) => params[&i],
+			Parameter::Binary(params) => panic!("capture_unary() not allowed to binary parameters!")
+		}
+	}
+	pub fn capture_binary(&self, i: u8, j: u8) -> f64 {
+		match self {
+			Parameter::Unary(params) => panic!("capture_binary() not allowed to unary parameters!"),
+			Parameter::Binary(params) => params[&(i, j)]
+		}
+	}
+}
+
