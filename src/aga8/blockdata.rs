5d5532a228bee02aa76c9f0cbda13b289f988ac5
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2023-10-17 10:21:57 -0300

    using snake_case for variables

--- src/aga8/blockdata.rs
+++ src/aga8/blockdata.rs
@@ -21,50 +21,50 @@ fn read_table(table_num: u8) -> Value {
 
 fn get_table4() -> (unary) {
 	let table_4 = read_table(4);
-	let mut A: HashMap<u8, f64> = HashMap::new();
+	let mut a: HashMap<u8, f64> = HashMap::new();
 	for cid in 1..=58 {
 		let value = *&table_4["data"][cid.to_string()]["a"].clone().as_f64().unwrap();
-		A.insert(cid, value);
+		a.insert(cid, value);
 	}
-	return (A);
+	return (a);
 }
 
 fn get_table5() -> (unary, unary, unary, unary, unary, unary, unary, unary) {
 	let table_5 = read_table(5);
 	let default: f64 = *&table_5["default_value"].clone().as_f64().unwrap();
-	let mut QIB: unary = HashMap::new();
-	let mut HIB: unary = HashMap::new();
-	let mut RKIB: unary = HashMap::new();
-	let mut EIB: unary = HashMap::new();
-	let mut WIB: unary = HashMap::new();
-	let mut CMWB: unary = HashMap::new();
-	let mut MIB: unary = HashMap::new();
-	let mut DIB: unary = HashMap::new();
+	let mut qib: unary = HashMap::new();
+	let mut hib: unary = HashMap::new();
+	let mut rkib: unary = HashMap::new();
+	let mut eib: unary = HashMap::new();
+	let mut wib: unary = HashMap::new();
+	let mut cmwb: unary = HashMap::new();
+	let mut mib: unary = HashMap::new();
+	let mut dib: unary = HashMap::new();
 	let fetch = | cid: u8, parameter: &str | -> f64 {
 		let index: String = cid.to_string();
 		let value: f64 = *&table_5["data"][index][parameter].clone().as_f64().unwrap_or(default);
 		return value;
 	};
 	for cid in 1..=21 {
-		QIB.insert(cid, fetch(cid, "Q"));
-		HIB.insert(cid, fetch(cid, "F"));
-		RKIB.insert(cid, fetch(cid, "K"));
-		EIB.insert(cid, fetch(cid, "E"));
-		WIB.insert(cid, fetch(cid, "G"));
-		CMWB.insert(cid, fetch(cid, "M"));
-		MIB.insert(cid, fetch(cid, "S"));
-		DIB.insert(cid, fetch(cid, "W"));
+		qib.insert(cid, fetch(cid, "Q"));
+		hib.insert(cid, fetch(cid, "F"));
+		rkib.insert(cid, fetch(cid, "K"));
+		eib.insert(cid, fetch(cid, "E"));
+		wib.insert(cid, fetch(cid, "G"));
+		cmwb.insert(cid, fetch(cid, "M"));
+		mib.insert(cid, fetch(cid, "S"));
+		dib.insert(cid, fetch(cid, "W"));
 	}
-	return (QIB, HIB, RKIB, EIB, WIB, CMWB, MIB, DIB);
+	return (qib, hib, rkib, eib, wib, cmwb, mib, dib);
 }
 
 fn get_table6() -> (binary, binary, binary, binary) {
 	let table_6 = read_table(6);
 	let default: f64 = *&table_6["default_value"].clone().as_f64().unwrap();
-	let mut BUIJB: binary = HashMap::new();
-	let mut BKIJB: binary = HashMap::new();
-	let mut BEIJB: binary = HashMap::new();
-	let mut BWIJB: binary = HashMap::new();
+	let mut buijb: binary = HashMap::new();
+	let mut bkijb: binary = HashMap::new();
+	let mut beijb: binary = HashMap::new();
+	let mut bwijb: binary = HashMap::new();
 	let fetch = | i_num: u8, j_num: u8, parameter: &str | -> f64 {
 		let i: String = i_num.to_string();
 		let j: String = j_num.to_string();
@@ -73,35 +73,35 @@ fn get_table6() -> (binary, binary, binary, binary) {
 	};
 	for i in 1..=20 {
 		for j in i+1..=21 {
-			BUIJB.insert((i, j), fetch(i, j, "U"));
-			BKIJB.insert((i, j), fetch(i, j, "K"));
-			BEIJB.insert((i, j), fetch(i, j, "E"));
-			BWIJB.insert((i, j), fetch(i, j, "G"));
+			buijb.insert((i, j), fetch(i, j, "U"));
+			bkijb.insert((i, j), fetch(i, j, "K"));
+			beijb.insert((i, j), fetch(i, j, "E"));
+			bwijb.insert((i, j), fetch(i, j, "G"));
 		}
 	}
-	return (BUIJB, BKIJB, BEIJB, BWIJB);
+	return (buijb, bkijb, beijb, bwijb);
 }
 
 pub fn blockdata() -> HashMap<&'static str, Parameter> {
 	//	Equation of state parameters
-	let (A) = get_table4();
+	let (a) = get_table4();
 	//	Individual Component Parameters
-	let (QIB, HIB, RKIB, EIB, WIB, CMWB, MIB, DIB) = get_table5();
-	let (BUIJ, BKIJB, BEIJB, BWIJB) = get_table6();
+	let (qib, hib, rkib, eib, wib, cmwb, mib, dib) = get_table5();
+	let (buij, bkijb, beijb, bwijb) = get_table6();
 	let data: HashMap<&str, Parameter> = HashMap::from([
-		("A", Parameter::Unary(A)),
-		("QIB", Parameter::Unary(QIB)),
-		("HIB", Parameter::Unary(HIB)),
-		("RKIB", Parameter::Unary(RKIB)),
-		("EIB", Parameter::Unary(EIB)),
-		("WIB", Parameter::Unary(WIB)),
-		("CMWB", Parameter::Unary(CMWB)),
-		("MIB", Parameter::Unary(MIB)),
-		("DIB", Parameter::Unary(DIB)),
-		("BUIJ", Parameter::Binary(BUIJ)),
-		("BKIJB", Parameter::Binary(BKIJB)),
-		("BEIJB", Parameter::Binary(BEIJB)),
-		("BWIJB", Parameter::Binary(BWIJB))
+		("A", Parameter::Unary(a)),
+		("QIB", Parameter::Unary(qib)),
+		("HIB", Parameter::Unary(hib)),
+		("RKIB", Parameter::Unary(rkib)),
+		("EIB", Parameter::Unary(eib)),
+		("WIB", Parameter::Unary(wib)),
+		("CMWB", Parameter::Unary(cmwb)),
+		("MIB", Parameter::Unary(mib)),
+		("DIB", Parameter::Unary(dib)),
+		("BUIJ", Parameter::Binary(buij)),
+		("BKIJB", Parameter::Binary(bkijb)),
+		("BEIJB", Parameter::Binary(beijb)),
+		("BWIJB", Parameter::Binary(bwijb))
 	]);
 	return data;
 }
