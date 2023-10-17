11ea0729cbcb8c88fed202c077261b155b49b0f9
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2023-10-17 00:11:48 -0300

    implementing ncc into paramdl

--- src/aga8/paramdl.rs
+++ src/aga8/paramdl.rs
@@ -1,5 +1,19 @@
 use std::collections::HashMap;
 
-pub fn paramdl(ncc: u8, cid: HashMap<u8, f64>) -> HashMap<u8, f64> {
+fn get_ncc(cid: HashMap<u8, f64>) -> u8 {
+	let mut ncc: u8 = 0;
+	for component in 1..=21 {
+		if cid[&component] > 0.0 {
+			ncc += 1;
+		} else {
+			break;
+		}
+	}
+	return ncc;
+}
+
+pub fn paramdl(cid: HashMap<u8, f64>) /*-> HashMap<u8, f64>*/ {
+	let ncc: u8 = get_ncc(cid);
+	dbg!(ncc);
 }
 
