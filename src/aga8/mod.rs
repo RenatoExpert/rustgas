42146c2604a261466da5603995244c7a2ac5c454
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2023-10-17 00:09:44 -0300

    reactivating paramdl, let ncc to be inside it

--- src/aga8/mod.rs
+++ src/aga8/mod.rs
@@ -1,29 +1,16 @@
 use std::collections::HashMap;
-//mod paramdl;
+mod paramdl;
 mod chardl;
 mod blockdata;
 
-fn get_ncc(cid: HashMap<u8, f64>) -> u8 {
-	let mut ncc: u8 = 0;
-	for component in 1..21 {
-		if cid[&component] > 0.0 {
-			ncc += 1;
-		} else {
-			break;
-		}
-	}
-	return ncc;
-}
-
 pub fn detail(cid: HashMap<u8, f64>, tk: f64, pmp: f64) {
 	let blockdata: HashMap<&'static str, blockdata::Parameter> = blockdata::blockdata();
-	let ncc = get_ncc(cid.clone());
-	//let params = paramdl::paramdl(ncc, cid.clone());
+	let params = paramdl::paramdl(cid);
 	//dbg!(params);
+	/*
 	let xi = cid.clone();
 	let (zb, db) = chardl::chardl(ncc, xi);
 	dbg!(zb, db);
-	/*
 	temp(tk);
 	d = ddetail(pmp, tk);
 	dbg!(d);
