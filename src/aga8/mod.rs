efd16ab977142dc3713d025b9ea4e5c3edf5e026
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2023-10-20 11:06:28 -0300

    changing chardl return values

--- src/aga8/mod.rs
+++ src/aga8/mod.rs
@@ -6,8 +6,8 @@ mod chardl;
 pub fn detail(cid: global::Unary, tk: f64, pmp: f64) {
 	let block_data: global::ParameterSet = blockdata::blockdata();
 	let params: global::ParameterSet = paramdl::paramdl(cid.clone(), block_data);
-	let (zb, db) = chardl::chardl(cid.clone(), params.clone());
-	dbg!(tk, pmp, zb, db);
+	let chardl = chardl::chardl(cid.clone(), params.clone());
+	dbg!(tk, pmp, chardl);
 	/*
 	let xi = cid.clone();
 	dbg!(zb, db);
